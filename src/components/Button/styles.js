330d50419e695cb5c8d35942b8553f95ba584949
Felipe Manoel Rodrigues Pessoa <felipemanuel.p@gmail.com>
2025-12-26 20:59:36 -0300

    feat

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 50px;
+    font-size: 24px;
+    border: 1px solid #ffffffff;
+    background-color: #3603eeff;
+    color: #ffffffff;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.8;
+        cursor: pointer;
+    }
+`
